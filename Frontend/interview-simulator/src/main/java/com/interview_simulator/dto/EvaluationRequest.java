59df41b37f008b08b8eee463c0b6636a9867ce0a
Sreeramula Indu <indusreeramula@gmail.com>
2025-08-02 18:36:31 +0530

    Delete Frontend directory

--- Frontend/interview-simulator/src/main/java/com/interview_simulator/dto/EvaluationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.interview_simulator.dto;
-
-import lombok.Data;
-
-@Data
-public class EvaluationRequest {
-    private String question;
-    private String response;
-}
--- Frontend/interview-simulator/src/main/java/com/interview_simulator/dto/InterviewRequest.java
+++
@@ -1,9 +0,0 @@
-package com.interview_simulator.dto;
-
-import lombok.Data;
-
-@Data
-public class InterviewRequest {
-    private String topic;
-    private String difficulty;
-}
